80765da413c5349c1e4368949e98cb353a465eef
Jonathan Shyaka <jonashyaka2@gmail.com>
2020-03-16 00:33:31 +0200

    Design a coming soon page

---
+++ src/App.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import './styles/App.scss';
+import Home from './views/Home';
+
+export default () => <Home />;
---
+++ src/views/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../styles/Home.scss';
+
+export default () => {
+  return (
+    <div className="app">
+      <header className="cover">
+        <div>
+          <span className="names">Jonathan Shyaka</span>
+          <br />
+          <br />
+          <span className="title">Software Developer</span>
+          <p className="coming-soon">Coming Soon...</p>
+        </div>
+      </header>
+    </div>
+  );
+};
