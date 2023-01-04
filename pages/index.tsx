297c9a6e996918f1f5b34bda072f8b6608f58290
Jonathan Shyaka <jonashyaka2@gmail.com>
2023-01-04 23:04:44 +0200

    Switch to Next.js

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+const Homepage = () => {
+  return (
+    <>
+      <Head>
+        <title>Jonathan Shyaka</title>
+      </Head>
+      <h1 className="text-3xl font-bold">
+        Jonathan Shyaka
+      </h1>
+    </>
+  );
+};
+
+export default Homepage;
--- src/App.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import './styles/App.scss';
-import Home from './views/Home';
-
-export default () => <Home />;
